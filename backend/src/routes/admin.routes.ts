2bbf38e95ba6e2beac50cff8cee583337d4ca29c
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-03-17 16:33:08 +0530

    update: updated routes

---
+++ backend/src/routes/admin.routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { adminLogin, adminSignup } from '../controllers/admin.controller.js';
+
+
+const router = express.Router();
+
+router.post("/signup", adminSignup); 
+router.post("/login", adminLogin); 
+
+export default router;
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controllers.js';
+import { authDelete, authUpdate, login, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.put("/update/:id", authUpdate);
+router.delete("/delete/:id", authDelete);
 
 export default router;
