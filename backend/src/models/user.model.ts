ebaeb1d1d1a8020fe5eb1aca6510e287c536fd2c
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-03-17 16:32:45 +0530

    chore: 'role: enum' is added for user and admin

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema } from "mongoose";
 interface IUser {
   email: string;
   password: string;
+  role?: string;
 }
 
 const userSchema = new Schema<IUser>({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "user"], default: "user" },
 });
 
 export default mongoose.model("User", userSchema);
