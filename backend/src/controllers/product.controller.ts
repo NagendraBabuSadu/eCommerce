42d4bc8758c6ad1bda919358564423a51ad3daf5
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-03-25 10:55:43 +0530

    chore: added mongoose

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import productModel from "../models/product.model.js";
 import { createProduct } from "../validators/product.validator.js";
+import mongoose from "mongoose";
+import {
+  getProductByName,
+  getProductsByCategory,
+} from "../middlewares/getProduct.js";
 
 const addProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -12,13 +17,12 @@ const addProduct = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     const newProduct = await productModel.create({
-        name: createPayload.name,
-        price: createPayload.price,
-        description: createPayload.description,
-        category: createPayload.category,
+      productName: createPayload.productName,
+      price: createPayload.price,
+      description: createPayload.description,
+      category: createPayload.category,
     });
 
-    
     await newProduct.save();
     res.status(200).json({ msg: "Product created", product: newProduct });
   } catch (error) {
@@ -26,4 +30,32 @@ const addProduct = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default addProduct;
+const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (req.query.name) {
+      getProductByName(req, res, next); // using getProductByName middleware
+    } else {
+      getProductsByCategory(req, res, next); // using getProductsByCategory middleware
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Error creating product", error });
+  }
+};
+
+const getProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { _id } = req.params;
+    if (mongoose.Types.ObjectId.isValid(_id)) {
+      const product = await productModel.findById(_id);
+
+      res.status(200).json({
+        msg: "success",
+        Product: product,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Error finding product", error });
+  }
+};
+
+export { addProduct, getProducts, getProduct, getProductByName };
