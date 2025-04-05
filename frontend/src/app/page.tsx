6cdf5b628c099eb9f24931c34878c187bbecf3bc
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-04-05 14:09:08 +0530

    chore: Just changed the variable name for the scrolling images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 import ImageCarousel from "./components/CarouselComponent";
 import { ProductList } from "./components/ProductList";
 
+
 export default function Home() {
-  const images = [
+  const scrollImages = [
     "https://rukminim2.flixcart.com/fk-p-flap/1620/270/image/3d7827c92a670177.jpg?q=20",
     "https://rukminim2.flixcart.com/fk-p-flap/1620/270/image/57d9b129e302642e.jpg?q=22",
     "https://rukminim2.flixcart.com/fk-p-flap/1620/270/image/a354077c3747d8f6.png?q=21",
   ];
 
   return (
-    <div className="grid grid-rows-[10px_2fr_10px] items-center justify-items-center min-h-screen p-1 pb-1 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center min-h-screen p-1 pb-1 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ImageCarousel images={images} />
+  
+        <ImageCarousel images={scrollImages} />
         <ProductList />
       </main>
     </div>
