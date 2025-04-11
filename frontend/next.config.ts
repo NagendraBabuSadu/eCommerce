980f33588b2084113528b609d3352cbf6fe570eb
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-04-11 11:42:47 +0530

    chore: added the feature of working on external web domain pages.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["img.freepik.com"], // ✅ Add the external image domain here
+  },
 };
 
 export default nextConfig;
-
-
