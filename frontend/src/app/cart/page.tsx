f975632e5775d0211992196965f57db9194f9df3
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-04-17 10:52:09 +0530

    chore: created cart page

---
+++ frontend/src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import axios from "axios";
+
+const CartPage: React.FC = () => {
+  
+
+  return (
+    <Box sx={{ maxWidth: "1200px", margin: "auto", mt: 4, p: 2 }}>
+      <Typography variant="h1">Cart</Typography>
+    </Box>
+  );
+};
+
+export default CartPage;
