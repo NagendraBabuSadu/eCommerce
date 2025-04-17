b433f86a8ded104e07e504fbb888ff76571039ac
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-04-17 10:55:41 +0530

    chore: created authMiddleware file

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+interface JwtPayload {
+  id: string;
+  email: string;
+  role: "user" | "admin";
+}
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    // Attach user to req
+    req.user = {
+      id: decoded.id,
+      email: decoded.email,
+      role: decoded.role,
+    };
+
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Invalid token" });
+  }
+};
